5e78cbf9cf2f43dbae4b1a28d564395f06df0086
Jon Haddad <jon@jonhaddad.com>
2020-04-26 14:30:50 -0700

    missing some script still

--- src/main/resources/com/thelastpickle/tlpcluster/commands/origin/provisioning/cassandra/70_flamegraphs.sh
+++ src/main/resources/com/thelastpickle/tlpcluster/commands/origin/provisioning/cassandra/70_flamegraphs.sh
@@ -4,13 +4,6 @@
 sudo bash -c "echo 1 > /proc/sys/kernel/perf_event_paranoid"
 sudo bash -c "echo 0 > /proc/sys/kernel/kptr_restrict"
 
-mkdir profiler
-cd profiler
 
-PROFILER="https://github.com/jvm-profiling-tools/async-profiler/releases/download/v1.7/async-profiler-1.7-linux-x64.tar.gz"
+tar -zxvf profiler.tgz
 
-wget $PROFILER
-
-tar -zxvf async-profiler*
-
-# sudo ./profiler.sh -d 30 -f /tmp/flamegraph.svg $(cat /var/run/cassandra/cassandra.pid)
