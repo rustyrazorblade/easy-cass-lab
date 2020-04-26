1921f66abeb800642a36fac8d5cc94c87b45d5bf
Jon Haddad <jon@jonhaddad.com>
2020-04-26 14:42:16 -0700

    flamegraphs

--- src/main/resources/com/thelastpickle/tlpcluster/commands/origin/provisioning/cassandra/70_flamegraphs.sh
+++ src/main/resources/com/thelastpickle/tlpcluster/commands/origin/provisioning/cassandra/70_flamegraphs.sh
@@ -4,6 +4,5 @@
 sudo bash -c "echo 1 > /proc/sys/kernel/perf_event_paranoid"
 sudo bash -c "echo 0 > /proc/sys/kernel/kptr_restrict"
 
-
-tar -zxvf profiler.tgz
+tar -zxvf profiler.tgz -C /usr/local/cassandra-profiler
 
