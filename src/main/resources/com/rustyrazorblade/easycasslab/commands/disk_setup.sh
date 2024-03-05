4d9f13316ed6137d7c2fe95355bbc6ec6916add7
Jon Haddad <jon@jonhaddad.com>
2024-03-05 06:13:25 -0800

    instance setup

--- src/main/resources/com/rustyrazorblade/easycasslab/commands/disk_setup.sh
+++ src/main/resources/com/rustyrazorblade/easycasslab/commands/disk_setup.sh
@@ -1,26 +1,71 @@
 #!/usr/bin/env bash
 
+###### CONFIGURATION ######
+
+#export DISK=/dev/nvme0n1
+export DISK=/dev/xvdb
+export READAHEAD=8
+
+###########################
+
+
+###### system settings #####
+
+sudo sysctl kernel.perf_event_paranoid=1
+sudo sysctl kernel.kptr_restrict=0
+
+echo 0 > /proc/sys/vm/zone_reclaim_mode
+
+cat <<EOF | sudo tee /etc/security/limits.d/cassandra.conf
+cassandra soft memlock unlimited
+cassandra hard memlock unlimited
+cassandra soft nofile 100000
+cassandra hard nofile 100000
+cassandra soft nproc 32768
+cassandra hard nproc 32768
+cassandra - as unlimited
+EOF
+
+cat <<EOF | sudo tee /etc/sysctl.d/60-cassandra.conf
+vm.max_map_count = 1048575
+EOF
+
+sudo swapoff --all
+
+sudo sysctl -p
+
+########
+
+
+#export DISK=/dev/xvdb
+
+# i3 instances have NVMe drives
+
 sudo mkdir -p /mnt/cassandra
 
-FS_TYPE=$(sudo blkid -o value -s TYPE "/dev/xvdb")
+FS_TYPE=$(sudo blkid -o value -s TYPE $DISK )
 
 if [ -z "$FS_TYPE" ]; then
-  echo "No file system found on /dev/xvdb. Formatting with XFS."
-  sudo mkfs.xfs /dev/xvdb
+  echo "No file system found on $DISK. Formatting with XFS."
+  sudo mkfs.xfs $DISK
 else
-  echo "File system found on /dev/xvdb. Not formatting."
+  echo "File system found on $DISK. Not formatting."
 fi
 
-sudo mount | grep "/dev/xvdb"
+sudo mount | grep $DISK
 
 if [ $? -eq 0 ]; then
     echo "$1 is mounted already."
 else
     echo "$1 is not mounted yet, mounting."
-    sudo mount /dev/xvdb /mnt/cassandra
+    sudo mount $DISK /mnt/cassandra
 fi
 
 sudo chown cassandra:cassandra /mnt/cassandra
+sudo blockdev --setra $READAHEAD $DISK
+
+echo
+
 # Here's some other ideas
 
 # multiple drives in a RAID
