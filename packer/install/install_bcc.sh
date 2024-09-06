92501510c298f2a2153924fb3196072968251be9
Jon Haddad <jon@jonhaddad.com>
2024-09-05 19:00:18 -0700

    upgraded bcc tools

--- packer/install/install_bcc.sh
+++ packer/install/install_bcc.sh
@@ -4,17 +4,17 @@ BCC_VERSION=0.31.0
 
 sudo apt update
 sudo apt purge bpfcc-tools libbpfcc python3-bpfcc
-wget https://github.com/iovisor/bcc/releases/download/v${BCC_VERSION}/bcc-src-with-submodule.tar.gz
-tar xf bcc-src-with-submodule.tar.gz
-cd bcc/
-
-
 sudo apt install -y zip bison build-essential cmake flex git libedit-dev \
   libllvm14 llvm-14-dev libclang-14-dev python3 zlib1g-dev libelf-dev libfl-dev python3-setuptools \
   liblzma-dev libdebuginfod-dev arping netperf iperf
 
+wget https://github.com/iovisor/bcc/releases/download/v${BCC_VERSION}/bcc-src-with-submodule.tar.gz
+tar xf bcc-src-with-submodule.tar.gz
+
+
 # from https://github.com/iovisor/bcc/blob/master/INSTALL.md
 
+cd bcc/
 sudo ln -s /usr/bin/python3 /usr/bin/python
 mkdir build
 cd build/
